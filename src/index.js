498db8a0b4b8a84d35f2496e206df8b038e54f77
KP <kpinc425@gmail.com>
2022-12-02 21:56:32 -0800

    Add dynamic styling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import './reset.css';
 import './style.css';
 import createImgSlider from './createImgSlider';
+import img1 from './media/img1.jpg';
+import img2 from './media/img2.jpg';
+import img3 from './media/img3.jpg';
+import img4 from './media/img4.jpg';
+import img5 from './media/img5.jpg';
+import img6 from './media/img6.jpg';
+import img7 from './media/img7.jpg';
 
 let imgArray = [
-    '../src/media/img1.jpg',
-    '../src/media/img2.jpg',
-    '../src/media/img3.jpg',
-    '../src/media/img4.jpg',
-    '../src/media/img5.jpg',
-    '../src/media/img6.jpg',
-    '../src/media/img7.jpg',
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
 ]
 
 createImgSlider(imgArray);
